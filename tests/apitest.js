fe0cfea3ea4e338d6aae24b74ce7cd55d16bd2c4
zxnii <61130784+Zxnii@users.noreply.github.com>
2020-10-23 19:17:11 -0600

    Fix issue with apitest.js

--- tests/apitest.js
+++ tests/apitest.js
@@ -5,9 +5,11 @@ const Scratch = require("../index")
 const user = new Scratch.User()
 
 const main = async () => {
-    let data = await user.login(credentials.username, credentials.password)
+    let data = await user.login(credentials.user, credentials.pass)
     console.log(data.status, data.json)
     data = await user.comments.commentOnUser("ez claps", "uwv")
+    console.log(data.status, data.json)
+    data = await user.comments.commentOnProject("nodejs test thing irdk", "419248967")
     console.log(data.status, data.json)
 }
 
